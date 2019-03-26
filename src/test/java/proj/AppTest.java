718a44a9945626fe0d47253b4610b6e305a6d40b
Chirtes Paul <cpie2088@scs.ubbcluj.ro>
2019-03-27 00:06:08 +0200

    push

---
+++ src/test/java/proj/AppTest.java
@@ -0,0 +1,57 @@
+package proj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import repository.NotaFileRepository;
+import repository.StudentFileRepository;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import repository.NotaXMLRepo;
+import repository.TemaFileRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void addStudentTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        try {
+            service.addStudent(new Student("1234", "Paul", 1,"a@a.com"));
+            assertTrue(true);
+        } catch (Exception ex){
+            assertTrue(false);
+        }
+    }
+}
