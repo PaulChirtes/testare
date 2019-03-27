9cbf4adea718a622296d288174524ba408def5bc
Chirtes Paul <cpie2088@scs.ubbcluj.ro>
2019-03-27 12:40:12 +0200

    test if works 6

--- src/test/java/proj/AppTest.java
+++ src/test/java/proj/AppTest.java
@@ -1,6 +1,7 @@
 package proj;
 
 import org.junit.Test;
+import proj.domain.Student;
 import proj.repository.NotaXMLRepo;
 import proj.repository.StudentXMLRepo;
 import proj.repository.TemaXMLRepo;
@@ -9,7 +10,9 @@ import proj.validation.NotaValidator;
 import proj.validation.StudentValidator;
 import proj.validation.TemaValidator;
 
-import static org.junit.Assert.assertTrue;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -32,17 +35,20 @@ public class AppTest
         String filenameStudent = "fisiere/Studenti.xml";
         String filenameTema = "fisiere/Teme.xml";
         String filenameNota = "fisiere/Note.xml";
-
-        //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
-        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
-        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
-        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
-
         StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
         TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
         NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
         NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
         Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        long countBefore = StreamSupport.stream(service.getAllStudenti().spliterator(), false).count();
+        service.addStudent(new Student("100", "Paul", 10, "a@a.a"));
+        long countAfter = StreamSupport.stream(service.getAllStudenti().spliterator(), false).count();
+        assertTrue(countBefore+1==countAfter);
+        Student stud = service.findStudent("100");
+        assertNotNull(stud);
+        service.deleteStudent("100");
+        long countAfterDelete = StreamSupport.stream(service.getAllStudenti().spliterator(), false).count();
+        assertEquals(countBefore, countAfterDelete);
         assertTrue(true);
     }
 }
