465489ef26d4b88784efff51f593f858cc312117
Chirtes Paul <cpie2088@scs.ubbcluj.ro>
2019-03-27 12:23:37 +0200

    test if works 2

--- src/test/java/proj/AppTest.java
+++ src/test/java/proj/AppTest.java
@@ -1,6 +1,14 @@
 package proj;
 
 import org.junit.Test;
+import proj.repository.NotaXMLRepo;
+import proj.repository.StudentXMLRepo;
+import proj.repository.TemaXMLRepo;
+import proj.service.Service;
+import proj.validation.NotaValidator;
+import proj.validation.StudentValidator;
+import proj.validation.TemaValidator;
+
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -19,22 +27,22 @@ public class AppTest
 
     @Test
     public void addStudentTest(){
-//        StudentValidator studentValidator = new StudentValidator();
-////        TemaValidator temaValidator = new TemaValidator();
-////        String filenameStudent = "fisiere/Studenti.xml";
-////        String filenameTema = "fisiere/Teme.xml";
-////        String filenameNota = "fisiere/Note.xml";
-////
-////        //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
-////        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
-////        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
-////        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
-////
-////        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
-////        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
-////        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
-////        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
-////        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
         assertTrue(true);
     }
 }
